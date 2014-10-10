8aff484104069891f5ebab4148f553adce7a7d33
Ty Overby <ty@pre-alpha.com>
2014-10-10 16:10:19 -0700

    refactored to use a searchstate

--- src/lib.rs
+++ src/lib.rs
@@ -5,27 +5,27 @@ use std::hash::Hash;
 use std::num::Num;
 use std::num::Zero;
 
-use reverse_ord::ReverseOrd as Rord;
+use node_wrapper::NodeWrapper as Nwrap;
 
 // Required for making a min-heap
 mod reverse_ord;
+mod node_wrapper;
 
-trait SearchState<N, C> {
+pub trait SearchState<N, C> {
     fn start(&self) -> N;
     fn end(&self) -> N;
     fn heuristic(&self, from: &N) -> C;
     fn neighbors(&self, at: &N) -> Vec<(N, C)>;
 }
 
-trait Node: Hash + Clone {}
-trait Cost: Num + Zero + PartialOrd + Clone {}
+pub trait Node: Hash + Clone + Eq {}
+pub trait Cost: Num + Zero + Ord + Clone {}
 
-pub fn astar<N: Ord + Hash + Clone, F: Num + Zero + PartialOrd + Clone>(
-        start: N,
-        end: N,
-        heuristic: |&N, &N| -> F,
-        neighbors: |&N| -> Vec<(N, F)>
-        ) -> Option<Vec<N>> {
+pub fn astar<N, C, S: SearchState<N, C>>(s: S) -> Option<Vec<N>>
+where N: Hash + Clone + Eq, C: Num + Zero + Ord + Clone {
+    let start = s.start();
+    let end = s.end();
+
     let mut closed_set: HashSet<N> = HashSet::new();
     let mut open_set = HashSet::new();
     let mut open_queue = PriorityQueue::new();
@@ -34,22 +34,21 @@ pub fn astar<N: Ord + Hash + Clone, F: Num + Zero + PartialOrd + Clone>(
     let mut came_from = HashMap::new();
 
     open_set.insert(start.clone());
-    open_queue.push(Rord::new(start.clone()));
-
-    let zero: F = Zero::zero();
+    let zero: C = Zero::zero();
+    open_queue.push(Nwrap::new(start.clone(), zero.clone()));
     g_score.insert(start.clone(), zero);
-    f_score.insert(start.clone(), heuristic(&start, &end));
+    f_score.insert(start.clone(), s.heuristic(&start));
 
     loop {
         let current = match open_queue.pop() {
-            Some(Rord{e: ref x}) if x == &end => { break; }
-            Some(Rord{e: x}) => x,
+            Some(Nwrap{n: ref x, ..}) if x == &end => { break; }
+            Some(Nwrap{n: x, ..}) => x,
             None => { return None; }
         };
         open_set.remove(&current);
         closed_set.insert(current.clone());
 
-        for (neighbor, dist) in neighbors(&current).into_iter() {
+        for (neighbor, dist) in s.neighbors(&current).into_iter() {
             if closed_set.contains(&neighbor) {
                 continue;
             }
@@ -60,11 +59,11 @@ pub fn astar<N: Ord + Hash + Clone, F: Num + Zero + PartialOrd + Clone>(
 
                 came_from.insert(neighbor.clone(), current.clone());
                 g_score.insert(neighbor.clone(), tentative_g_score.clone());
-                f_score.insert(neighbor.clone(), tentative_g_score +
-                               heuristic(&neighbor, &end));
+                let total = tentative_g_score + s.heuristic(&neighbor);
+                f_score.insert(neighbor.clone(), total.clone());
                 if open_set.contains(&neighbor) {
                     open_set.insert(neighbor.clone());
-                    open_queue.push(Rord::new(neighbor))
+                    open_queue.push(Nwrap::new(neighbor, total));
                 }
             }
         }
