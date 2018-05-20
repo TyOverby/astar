3dfcc07900081a97684a497dfc42e762188f701f
Ty Overby <ty@pre-alpha.com>
2018-05-20 14:30:06 -0700

    remove weird old traits, add cost to output

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,16 @@
 #![allow(mutable_transmutes)]
+extern crate fnv;
 extern crate num_traits;
 extern crate typed_arena;
 
-use typed_arena::Arena as TypedArena;
-use std::vec::IntoIter;
+use fnv::FnvHashMap as HashMap;
 use num_traits::Zero;
-use std::hash::Hash;
+use std::cell::{Cell, RefCell};
+use std::cmp::Ordering;
 use std::collections::BinaryHeap;
 use std::collections::VecDeque;
-use std::collections::HashMap;
-use std::cmp::Ordering;
-use std::cell::{Cell, RefCell};
+use std::hash::Hash;
+use typed_arena::Arena as TypedArena;
 
 #[cfg(test)]
 mod test;
@@ -18,171 +18,20 @@ mod test;
 pub trait SearchProblem {
     type Node: Hash + PartialEq + Eq;
     type Cost: PartialOrd + Zero + Clone;
-    type Iter: Iterator<Item=(Self::Node, Self::Cost)>;
+    type Iter: Iterator<Item = (Self::Node, Self::Cost)>;
 
     fn start(&self) -> Self::Node;
     fn is_end(&self, &Self::Node) -> bool;
     fn heuristic(&self, &Self::Node) -> Self::Cost;
-    fn neighbors(&mut self, &Self::Node) -> Self::Iter;
+    fn neighbors(&self, &Self::Node) -> Self::Iter;
 
-    fn estimate_length(&self) -> Option<u32> { None }
-}
-
-pub trait ReusableSearchProblem {
-    type Node: Hash + PartialEq + Eq + Clone;
-    type Cost: PartialOrd + Zero + Clone;
-    type Iter: Iterator<Item=(Self::Node, Self::Cost)>;
-
-    fn heuristic(&self, &Self::Node, &Self::Node) -> Self::Cost;
-    fn neighbors(&mut self, &Self::Node) -> Self::Iter;
-    fn estimate_length(&self, _a: &Self::Node, _b: &Self::Node) -> Option<u32> { None }
-
-    fn search(&mut self, start: Self::Node, end: Self::Node) -> ReuseSearchInstance<Self, Self::Node> {
-        let est = self.estimate_length(&start, &end);
-        ReuseSearchInstance {
-            rsp: self,
-            start: start,
-            end: end,
-            estimation: est
-        }
+    fn estimate_length(&self) -> Option<u32> {
+        None
     }
-}
-
-pub trait TwoDSearchProblem {
-    fn get(&mut self, x: i32, y: i32) -> Option<u32>;
-    fn diag(&self) -> bool { false }
-    fn cut_corners(&self) ->  bool { false }
-}
-
-pub struct ReuseSearchInstance<'a, RSP: 'a + ?Sized, S> {
-    rsp: &'a mut RSP,
-    start: S,
-    end: S,
-    estimation: Option<u32>
-}
-
-impl <T: TwoDSearchProblem> ReusableSearchProblem for T {
-    type Node = (i32, i32);
-    type Cost = u32;
-    type Iter = IntoIter<((i32, i32), u32)>;
-
-    fn heuristic(&self, a: &Self::Node, b: &Self::Node) -> Self::Cost {
-        fn abs(x: i32) -> i32 {
-            if x < 0 { -x  } else { x }
-        }
-
-        let &(bx, by) = a;
-        let &(ex, ey) = b;
-        let (dx, dy) = (ex - bx, ey - by);
-
-        // Chebyshev Distance
-        // return (max(abs(dx), abs(dy)) * 2) as u32;
-        // Manhattan Distance
-        return ((abs(dx) + abs(dy)) as u32) * 2;
-    }
-
-    fn neighbors(&mut self, node: &Self::Node) -> Self::Iter {
-                let mut v = vec![];
-        let (x, y) = *node;
-        let ap = (x + 0, y + 1);
-        let bp = (x - 1, y + 0);
-        let cp = (x + 1, y + 0);
-        let dp = (x + 0, y - 1);
-
-        let a = self.get(ap.0, ap.1);
-        let b = self.get(bp.0, bp.1);
-        let c = self.get(cp.0, cp.1);
-        let d = self.get(dp.0, dp.1);
-
-        if let Some(p) = a {
-            v.push((ap, p + 2));
-        }
-        if let Some(p) = b {
-            v.push((bp, p + 2));
-        }
-        if let Some(p) = c {
-            v.push((cp, p + 2));
-        }
-        if let Some(p) = d {
-            v.push((dp, p + 2));
-        }
-
-        if self.diag() {
-            let xp = (x - 1, y + 1);
-            let yp = (x + 1, y + 1);
-            let zp = (x - 1, y - 1);
-            let wp = (x + 1, y - 1);
-
-            if !self.cut_corners() {
-                if a.is_some() && b.is_some() {
-                    if let Some(p) = self.get(xp.0, xp.1) {
-                        v.push((xp, p + 3));
-                    }
-                }
-                if a.is_some() && c.is_some() {
-                    if let Some(p) = self.get(yp.0, yp.1) {
-                        v.push((yp, p + 3));
-                    }
-                }
-                if c.is_some() && d.is_some() {
-                    if let Some(p) = self.get(wp.0, wp.1) {
-                        v.push((wp, p + 3));
-                    }
-                }
-                if b.is_some() && d.is_some() {
-                    if let Some(p) = self.get(zp.0, zp.1) {
-                        v.push((zp, p + 3))
-                    }
-                }
-
-            } else {
-                if let Some(p) = self.get(xp.0, xp.1) {
-                    v.push((xp, p + 3));
-                }
-                if let Some(p) = self.get(yp.0, yp.1) {
-                    v.push((yp, p + 3));
-                }
-                if let Some(p) = self.get(wp.0, wp.1) {
-                    v.push((wp, p + 3));
-                }
-                if let Some(p) = self.get(zp.0, zp.1) {
-                    v.push((zp, p + 3))
-                }
-            }
-        }
-
-        v.into_iter()
-    }
-
-    fn estimate_length(&self, _a: &Self::Node, _b: &Self::Node) -> Option<u32> { None }
-}
-
-impl <'a, RSP: ReusableSearchProblem> SearchProblem for ReuseSearchInstance<'a, RSP, RSP::Node> {
-    type Node = RSP::Node;
-    type Cost = RSP::Cost;
-    type Iter = RSP::Iter;
-
-    fn start(&self) -> Self::Node {
-         self.start.clone()
-    }
-
-    fn is_end(&self, other: &Self::Node) -> bool {
-        &self.end == other
-    }
-
-    fn heuristic(&self, a: &Self::Node) -> Self::Cost {
-        self.rsp.heuristic(a, &self.end)
-    }
-
-    fn neighbors(&mut self, node: &Self::Node) -> Self::Iter {
-        self.rsp.neighbors(node)
-    }
-
-    fn estimate_length(&self) -> Option<u32> { self.estimation }
 }
 
 #[derive(Debug)]
-struct SearchNode<'a: 'b, 'b, S: 'a , C: Clone + 'a> {
+struct SearchNode<'a: 'b, 'b, S: 'a, C: Clone + 'a> {
     pub state: &'a S,
     pub parent: RefCell<Option<&'b SearchNode<'a, 'b, S, C>>>,
 
@@ -194,7 +43,7 @@ struct SearchNode<'a: 'b, 'b, S: 'a , C: Clone + 'a> {
     pub closed: Cell<bool>,
 }
 
-impl <'a, 'b, S, C: Zero + Clone> SearchNode<'a, 'b, S, C> {
+impl<'a, 'b, S, C: Zero + Clone> SearchNode<'a, 'b, S, C> {
     fn new_initial(state: &'a S) -> SearchNode<S, C> {
         SearchNode {
             state: state,
@@ -203,7 +52,7 @@ impl <'a, 'b, S, C: Zero + Clone> SearchNode<'a, 'b, S, C> {
             f: RefCell::new(Zero::zero()),
             h: RefCell::new(Zero::zero()),
             opened: Cell::new(true),
-            closed: Cell::new(false)
+            closed: Cell::new(false),
         }
     }
 
@@ -215,7 +64,7 @@ impl <'a, 'b, S, C: Zero + Clone> SearchNode<'a, 'b, S, C> {
             f: RefCell::new(Zero::zero()),
             h: RefCell::new(Zero::zero()),
             opened: Cell::new(false),
-            closed: Cell::new(false)
+            closed: Cell::new(false),
         }
     }
 
@@ -244,13 +93,13 @@ impl <'a, 'b, S, C: Zero + Clone> SearchNode<'a, 'b, S, C> {
     }
 }
 
-impl <'a, 'b, S: PartialEq, C: Clone> PartialEq for SearchNode<'a, 'b, S, C> {
+impl<'a, 'b, S: PartialEq, C: Clone> PartialEq for SearchNode<'a, 'b, S, C> {
     fn eq(&self, other: &SearchNode<S, C>) -> bool {
         self.state.eq(&other.state)
     }
 }
 
-impl <'a, 'b, S: PartialEq, C: Clone> Eq for SearchNode<'a, 'b, S, C> {}
+impl<'a, 'b, S: PartialEq, C: Clone> Eq for SearchNode<'a, 'b, S, C> {}
 
 impl<'a, 'b, S: PartialEq, C: PartialOrd + Clone> PartialOrd for SearchNode<'a, 'b, S, C> {
     fn partial_cmp(&self, other: &SearchNode<S, C>) -> Option<Ordering> {
@@ -262,21 +111,25 @@ impl<'a, 'b, S: PartialEq, C: PartialOrd + Clone> Ord for SearchNode<'a, 'b, S, 
     fn cmp(&self, other: &SearchNode<'a, 'b, S, C>) -> Ordering {
         match self.partial_cmp(other) {
             Some(x) => x,
-            None => Ordering::Equal
+            None => Ordering::Equal,
         }
     }
 }
 
-// We aren't correctly closing items and they are getting popped multiple times.
-pub fn astar<S: SearchProblem>(s: &mut S) -> Option<VecDeque<S::Node>> where S::Node: ::std::clone::Clone + ::std::fmt::Debug, S::Cost: ::std::fmt::Debug {
-    let state_arena: TypedArena<S::Node>  = TypedArena::new();
+pub fn astar<S: SearchProblem>(s: &S) -> Option<(VecDeque<S::Node>, S::Cost)>
+where
+    S::Node: ::std::clone::Clone + ::std::fmt::Debug,
+    S::Cost: ::std::fmt::Debug,
+{
+    let state_arena: TypedArena<S::Node> = TypedArena::new();
     let node_arena: TypedArena<SearchNode<S::Node, S::Cost>> = TypedArena::new();
 
-    let mut state_to_node: HashMap<&S::Node, &SearchNode<S::Node, S::Cost>> = HashMap::new();
+    let mut state_to_node: HashMap<&S::Node, &SearchNode<S::Node, S::Cost>> = HashMap::default();
     let mut heap: BinaryHeap<&SearchNode<S::Node, S::Cost>> = BinaryHeap::new();
 
     let start_state: &S::Node = state_arena.alloc(s.start());
-    let start_node: &SearchNode<S::Node, S::Cost> = node_arena.alloc(SearchNode::new_initial(start_state));
+    let start_node: &SearchNode<S::Node, S::Cost> =
+        node_arena.alloc(SearchNode::new_initial(start_state));
 
     state_to_node.insert(start_state, start_node);
     heap.push(start_node);
@@ -295,10 +148,10 @@ pub fn astar<S: SearchProblem>(s: &mut S) -> Option<VecDeque<S::Node>> where S::
         }
 
         for (neighbor, cost) in s.neighbors(node_state) {
-            let neighbor_state:&_ = state_arena.alloc(neighbor);
-            let neighbor_node =
-                state_to_node.entry(neighbor_state)
-                             .or_insert_with(|| node_arena.alloc(SearchNode::new(neighbor_state)));
+            let neighbor_state: &_ = state_arena.alloc(neighbor);
+            let neighbor_node = state_to_node
+                .entry(neighbor_state)
+                .or_insert_with(|| node_arena.alloc(SearchNode::new(neighbor_state)));
 
             if neighbor_node.closed.get() {
                 continue;
@@ -323,14 +176,15 @@ pub fn astar<S: SearchProblem>(s: &mut S) -> Option<VecDeque<S::Node>> where S::
                 } else {
                     // We reset the value that did sorting.  This forces a
                     // recalculation.
-                    heap = heap_from_vec(vec_from_heap(heap)) // BinaryHeap::from_vec(heap.into_vec());
+                    resort_heap(&mut heap);
                 }
             }
         }
     }
 
-    if found.is_some() {
-        let mut prev = found;
+    if let Some(found) = found {
+        let cost = found.g();
+        let mut prev = Some(found);
         let mut deque = VecDeque::new();
 
         while let Some(node) = prev {
@@ -338,18 +192,16 @@ pub fn astar<S: SearchProblem>(s: &mut S) -> Option<VecDeque<S::Node>> where S::
             prev = node.parent.borrow_mut().take();
         }
 
-        Some(deque)
+        Some((deque, cost))
     } else {
         None
     }
 }
 
-fn heap_from_vec<T: Ord>(v: Vec<T>) -> BinaryHeap<T> {
-    let mut b_heap = BinaryHeap::with_capacity(v.len());
-    b_heap.extend(v.into_iter());
-    b_heap
-}
-
-fn vec_from_heap<T: Ord>(h: BinaryHeap<T>) -> Vec<T> {
-    h.into_iter().collect()
+fn resort_heap<T: Ord>(b: &mut BinaryHeap<T>) {
+    use std::mem::swap;
+    let mut temp = BinaryHeap::new();
+    swap(b, &mut temp);
+    temp = BinaryHeap::from(temp.into_sorted_vec());
+    swap(b, &mut temp);
 }
--- src/test.rs
+++ src/test.rs
@@ -1,10 +1,181 @@
-use super::{astar, SearchProblem, TwoDSearchProblem, ReusableSearchProblem};
+use super::{astar, SearchProblem};
+use num_traits::Zero;
 use std::collections::VecDeque;
+use std::hash::Hash;
 use std::vec::IntoIter;
 
+pub trait ReusableSearchProblem {
+    type Node: Hash + PartialEq + Eq + Clone;
+    type Cost: PartialOrd + Zero + Clone;
+    type Iter: Iterator<Item = (Self::Node, Self::Cost)>;
+
+    fn heuristic(&self, &Self::Node, &Self::Node) -> Self::Cost;
+    fn neighbors(&self, &Self::Node) -> Self::Iter;
+    fn estimate_length(&self, _a: &Self::Node, _b: &Self::Node) -> Option<u32> {
+        None
+    }
+
+    fn search(
+        &mut self,
+        start: Self::Node,
+        end: Self::Node,
+    ) -> ReuseSearchInstance<Self, Self::Node> {
+        let est = self.estimate_length(&start, &end);
+        ReuseSearchInstance {
+            rsp: self,
+            start: start,
+            end: end,
+            estimation: est,
+        }
+    }
+}
+
+pub trait TwoDSearchProblem {
+    fn get(&self, x: i32, y: i32) -> Option<u32>;
+    fn diag(&self) -> bool {
+        false
+    }
+    fn cut_corners(&self) -> bool {
+        false
+    }
+}
+
+pub struct ReuseSearchInstance<'a, RSP: 'a + ?Sized, S> {
+    rsp: &'a RSP,
+    start: S,
+    end: S,
+    estimation: Option<u32>,
+}
+
+impl<T: TwoDSearchProblem> ReusableSearchProblem for T {
+    type Node = (i32, i32);
+    type Cost = u32;
+    type Iter = IntoIter<((i32, i32), u32)>;
+
+    fn heuristic(&self, a: &Self::Node, b: &Self::Node) -> Self::Cost {
+        fn abs(x: i32) -> i32 {
+            if x < 0 {
+                -x
+            } else {
+                x
+            }
+        }
+
+        let &(bx, by) = a;
+        let &(ex, ey) = b;
+        let (dx, dy) = (ex - bx, ey - by);
+
+        // Chebyshev Distance
+        // return (max(abs(dx), abs(dy)) * 2) as u32;
+        // Manhattan Distance
+        return ((abs(dx) + abs(dy)) as u32) * 2;
+    }
+
+    fn neighbors(&self, node: &Self::Node) -> Self::Iter {
+        let mut v = vec![];
+        let (x, y) = *node;
+        let ap = (x + 0, y + 1);
+        let bp = (x - 1, y + 0);
+        let cp = (x + 1, y + 0);
+        let dp = (x + 0, y - 1);
+
+        let a = self.get(ap.0, ap.1);
+        let b = self.get(bp.0, bp.1);
+        let c = self.get(cp.0, cp.1);
+        let d = self.get(dp.0, dp.1);
+
+        if let Some(p) = a {
+            v.push((ap, p + 2));
+        }
+        if let Some(p) = b {
+            v.push((bp, p + 2));
+        }
+        if let Some(p) = c {
+            v.push((cp, p + 2));
+        }
+        if let Some(p) = d {
+            v.push((dp, p + 2));
+        }
+
+        if self.diag() {
+            let xp = (x - 1, y + 1);
+            let yp = (x + 1, y + 1);
+            let zp = (x - 1, y - 1);
+            let wp = (x + 1, y - 1);
+
+            if !self.cut_corners() {
+                if a.is_some() && b.is_some() {
+                    if let Some(p) = self.get(xp.0, xp.1) {
+                        v.push((xp, p + 3));
+                    }
+                }
+                if a.is_some() && c.is_some() {
+                    if let Some(p) = self.get(yp.0, yp.1) {
+                        v.push((yp, p + 3));
+                    }
+                }
+                if c.is_some() && d.is_some() {
+                    if let Some(p) = self.get(wp.0, wp.1) {
+                        v.push((wp, p + 3));
+                    }
+                }
+                if b.is_some() && d.is_some() {
+                    if let Some(p) = self.get(zp.0, zp.1) {
+                        v.push((zp, p + 3))
+                    }
+                }
+            } else {
+                if let Some(p) = self.get(xp.0, xp.1) {
+                    v.push((xp, p + 3));
+                }
+                if let Some(p) = self.get(yp.0, yp.1) {
+                    v.push((yp, p + 3));
+                }
+                if let Some(p) = self.get(wp.0, wp.1) {
+                    v.push((wp, p + 3));
+                }
+                if let Some(p) = self.get(zp.0, zp.1) {
+                    v.push((zp, p + 3))
+                }
+            }
+        }
+
+        v.into_iter()
+    }
+
+    fn estimate_length(&self, _a: &Self::Node, _b: &Self::Node) -> Option<u32> {
+        None
+    }
+}
+
+impl<'a, RSP: ReusableSearchProblem> SearchProblem for ReuseSearchInstance<'a, RSP, RSP::Node> {
+    type Node = RSP::Node;
+    type Cost = RSP::Cost;
+    type Iter = RSP::Iter;
+
+    fn start(&self) -> Self::Node {
+        self.start.clone()
+    }
+
+    fn is_end(&self, other: &Self::Node) -> bool {
+        &self.end == other
+    }
+
+    fn heuristic(&self, a: &Self::Node) -> Self::Cost {
+        self.rsp.heuristic(a, &self.end)
+    }
+
+    fn neighbors(&self, node: &Self::Node) -> Self::Iter {
+        self.rsp.neighbors(node)
+    }
+
+    fn estimate_length(&self) -> Option<u32> {
+        self.estimation
+    }
+}
 struct GridState {
     start: (i32, i32),
-    end: (i32, i32)
+    end: (i32, i32),
 }
 
 fn abs(x: i32) -> i32 {
@@ -20,16 +191,20 @@ impl SearchProblem for GridState {
     type Cost = i32;
     type Iter = IntoIter<((i32, i32), i32)>;
 
-    fn start(&self) -> (i32, i32) { self.start }
-    fn is_end(&self, other: &(i32, i32)) -> bool { other == &self.end }
+    fn start(&self) -> (i32, i32) {
+        self.start
+    }
+    fn is_end(&self, other: &(i32, i32)) -> bool {
+        other == &self.end
+    }
     fn heuristic(&self, &(p_x, p_y): &(i32, i32)) -> i32 {
         let (s_x, s_y) = self.end;
         abs(s_x - p_x) + abs(s_y - p_y)
     }
-    fn neighbors(&mut self, &(x, y): &(i32, i32)) -> IntoIter<((i32, i32), i32)> {
+    fn neighbors(&self, &(x, y): &(i32, i32)) -> IntoIter<((i32, i32), i32)> {
         let mut vec = vec![];
-        for i in -1 .. 1 + 1 {
-            for k in -1 .. 1 + 1 {
+        for i in -1..1 + 1 {
+            for k in -1..1 + 1 {
                 if !(i == 0 && k == 0) {
                     vec.push(((x + i, y + k), 1));
                 }
@@ -39,53 +214,68 @@ impl SearchProblem for GridState {
     }
 }
 
-fn path(start: (i32, i32), end: (i32, i32)) -> Option<VecDeque<(i32, i32)>> {
-    let mut gs = GridState{ start: start, end: end };
-    astar(&mut gs)
+fn path(start: (i32, i32), end: (i32, i32)) -> Option<(VecDeque<(i32, i32)>, i32)> {
+    let gs = GridState {
+        start: start,
+        end: end,
+    };
+    astar(&gs)
 }
 
 #[test]
 fn test_iter() {
-    let mut gs = GridState{ start: (0,0), end: (0,0) };
+    let gs = GridState {
+        start: (0, 0),
+        end: (0, 0),
+    };
     assert!(
-        gs.neighbors(&(0,0)).collect::<Vec<_>>() ==
-        vec![
-            ((-1, -1), 1),
-            ((-1, 0), 1),
-            ((-1, 1), 1),
-            ((0, -1), 1),
-            ((0, 1), 1),
-            ((1, -1), 1),
-            ((1, 0), 1),
-            ((1, 1), 1)
-        ])
+        gs.neighbors(&(0, 0)).collect::<Vec<_>>()
+            == vec![
+                ((-1, -1), 1),
+                ((-1, 0), 1),
+                ((-1, 1), 1),
+                ((0, -1), 1),
+                ((0, 1), 1),
+                ((1, -1), 1),
+                ((1, 0), 1),
+                ((1, 1), 1),
+            ]
+    )
 }
 
 #[test]
 fn test_start_end() {
-    let p = path((0,0), (0,0)).unwrap();
-    assert_eq!(p, vec![(0, 0)].into_iter().collect::<VecDeque<_>>());
+    let p = path((0, 0), (0, 0)).unwrap();
+    assert_eq!(p.0, vec![(0, 0)].into_iter().collect::<VecDeque<_>>());
 }
 
 #[test]
 fn test_next() {
-    let p = path((0,0), (0,1)).unwrap();
-    assert_eq!(p, vec![(0,0), (0,1)].into_iter().collect::<VecDeque<_>>());
+    let p = path((0, 0), (0, 1)).unwrap();
+    assert_eq!(
+        p.0,
+        vec![(0, 0), (0, 1)].into_iter().collect::<VecDeque<_>>()
+    );
 }
 
 #[test]
 fn test_few() {
-    let p = path((0,0), (0,4)).unwrap();
-    assert_eq!(p, vec![(0,0), (0,1) ,(0,2), (0,3), (0,4)].into_iter().collect::<VecDeque<_>>());
+    let p = path((0, 0), (0, 4)).unwrap();
+    assert_eq!(
+        p.0,
+        vec![(0, 0), (0, 1), (0, 2), (0, 3), (0, 4)]
+            .into_iter()
+            .collect::<VecDeque<_>>()
+    );
 }
 
 struct Maze {
     xmax: i32,
-    ymax: i32
+    ymax: i32,
 }
 
 impl TwoDSearchProblem for Maze {
-    fn get(&mut self, x: i32, y: i32) -> Option<u32> {
+    fn get(&self, x: i32, y: i32) -> Option<u32> {
         /* Imagine a simple maze, that looks something like this:
         .
         ........
@@ -109,7 +299,7 @@ impl TwoDSearchProblem for Maze {
 
 #[test]
 fn test_maze() {
-    let mut maze = Maze{ xmax: 7, ymax: 5 };
+    let mut maze = Maze { xmax: 7, ymax: 5 };
     /* If this test fails, try printing out the maze using this code:
     println!("");
     for y in 0 .. maze.ymax+1 {
@@ -122,27 +312,46 @@ fn test_maze() {
         println!("");
     }
     */
-    let p = astar(&mut maze.search((0,0), (0,4))).unwrap();
-    assert_eq!(p, vec![
-        (0, 0),
-        (0, 1), (1, 1), (2, 1), (3, 1), (4, 1), (5, 1), (6, 1), (7, 1),
-        (7, 2),
-        (7, 3), (6, 3), (5, 3), (4, 3), (3, 3), (2, 3), (1, 3), (0, 3),
-        (0, 4)].into_iter().collect::<VecDeque<_>>());
+    let p = astar(&mut maze.search((0, 0), (0, 4))).unwrap();
+    assert_eq!(
+        p.0,
+        vec![
+            (0, 0),
+            (0, 1),
+            (1, 1),
+            (2, 1),
+            (3, 1),
+            (4, 1),
+            (5, 1),
+            (6, 1),
+            (7, 1),
+            (7, 2),
+            (7, 3),
+            (6, 3),
+            (5, 3),
+            (4, 3),
+            (3, 3),
+            (2, 3),
+            (1, 3),
+            (0, 3),
+            (0, 4),
+        ].into_iter()
+            .collect::<VecDeque<_>>()
+    );
 }
 
 #[test]
 fn test_maze_reusable() {
-    let mut maze = Maze{ xmax: 7, ymax: 5 };
-    let p = astar(&mut maze.search((0,0), (0,4))).unwrap();
-    let p2 = astar(&mut maze.search((0,0), (0,4))).unwrap();
+    let mut maze = Maze { xmax: 7, ymax: 5 };
+    let p = astar(&mut maze.search((0, 0), (0, 4))).unwrap();
+    let p2 = astar(&mut maze.search((0, 0), (0, 4))).unwrap();
     assert_eq!(p, p2);
 }
 
 #[test]
 fn test_maze_reverse() {
-    let mut maze = Maze{ xmax: 7, ymax: 5 };
-    let p = astar(&mut maze.search((0,0), (0,4))).unwrap();
-    let p2 = astar(&mut maze.search((0,4), (0,0))).unwrap();
-    assert_eq!(p, p2.into_iter().rev().collect::<VecDeque<_>>());
+    let mut maze = Maze { xmax: 7, ymax: 5 };
+    let p = astar(&mut maze.search((0, 0), (0, 4))).unwrap();
+    let p2 = astar(&mut maze.search((0, 4), (0, 0))).unwrap();
+    assert_eq!(p.0, p2.0.into_iter().rev().collect::<VecDeque<_>>());
 }
